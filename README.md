e4db42181b7927c54e5215be0aebe3f253988c2c
algins <igors.algins@gmail.com>
2021-01-09 13:49:32 +0200

    add brain-progression game

--- README.md
+++ README.md
@@ -13,3 +13,4 @@
 * [Even](https://asciinema.org/a/S9WD17tSsjID3Vf5Stl6GV61b)
 * [Calc](https://asciinema.org/a/ukspYTyMtlBNFwzrY2euHN3XM)
 * [Gcd](https://asciinema.org/a/ThbcRToULMiDNQ2Pzf5sKSIXk)
+* [Progression](https://asciinema.org/a/PHIzWTNBrH27yxBGUpUrwXZJ6)
---
+++ src/Games/Progression.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace BrainGames\Games\Progression;
+
+use function BrainGames\Engine\handle as startGame;
+
+const GAME_DESCRIPTION = 'What number is missing in the progression?';
+const MAX_PROGRESSION_START_VALUE = 10;
+const MAX_PROGRESSION_STEP = 5;
+const PROGRESSION_SIZE = 10;
+
+function handle()
+{
+    $game = function () {
+        $start = mt_rand(1, MAX_PROGRESSION_START_VALUE);
+        $step = mt_rand(1, MAX_PROGRESSION_STEP);
+        $progression = generateProgression($start, $step, PROGRESSION_SIZE);
+        $key = getRandomKey($progression);
+        $answer = $progression[$key];
+        $progression[$key] = '..';
+        $question = implode(' ', $progression);
+
+        return [
+            'question' => $question,
+            'answer' => "{$answer}",
+        ];
+    };
+
+    startGame($game, GAME_DESCRIPTION);
+}
+
+function generateProgression($start, $step, $size)
+{
+    $progression = [];
+
+    for ($i = 0; $i < $size; $i += 1) {
+        $progression[$i] = $start + $step * $i;
+    }
+
+    return $progression;
+}
+
+function getRandomKey(array $arr)
+{
+    return array_rand($arr, 1);
+}
