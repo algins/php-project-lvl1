aaa23e428640f5801181b3311bd475e3fc9d859f
algins <igors.algins@gmail.com>
2021-01-10 11:52:21 +0200

    add Cli.php

---
+++ src/Cli.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BrainGames\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function handle()
+{
+    line('Welcome to the Brain Games!');
+    $playerName = prompt('May I have your name?');
+    line("Hello, %s!", $playerName);
+}
