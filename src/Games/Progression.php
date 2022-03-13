ca50703270197b7e21541e04a90141f6d9685943
algins <igors.algins@gmail.com>
2022-03-13 10:58:27 +0200

    generate progression with already hidden item

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -14,10 +14,9 @@ function handle(): void
     $game = function (): array {
         $start = mt_rand(1, MAX_PROGRESSION_START_VALUE);
         $step = mt_rand(1, MAX_PROGRESSION_STEP);
-        $progression = generateProgression($start, $step, PROGRESSION_SIZE);
-        $key = array_rand($progression);
-        $answer = $progression[$key];
-        $progression[$key] = '..';
+        $hiddenIndex = mt_rand(0, PROGRESSION_SIZE - 1);
+        $progression = generateProgression($start, $step, PROGRESSION_SIZE, $hiddenIndex);
+        $answer = $hiddenIndex === 0 ? $progression[1] - $step : $progression[$hiddenIndex - 1] + $step;
 
         return [
             'question' => implode(' ', $progression),
@@ -28,12 +27,12 @@ function handle(): void
     startGame($game, GAME_DESCRIPTION);
 }
 
-function generateProgression(int $start, int $step, int $size): array
+function generateProgression(int $start, int $step, int $size, int $hiddenIndex): array
 {
     $progression = [];
 
     for ($i = 0; $i < $size; $i += 1) {
-        $progression[$i] = $start + $step * $i;
+        $progression[$i] = $i === $hiddenIndex ? '..' : $start + $step * $i;
     }
 
     return $progression;
